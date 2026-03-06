733053067a3a8baf96a7ba05e970b038105fd567
Rachit Kumar Verma <155641117+rachitvrma@users.noreply.github.com>
2026-03-06 10:48:11 +0530

    type(wrapperModules.aria2): init (#319)

---
+++ wrapperModules/a/aria2/check.nix
@@ -0,0 +1,21 @@
+{
+  pkgs,
+  self,
+}:
+let
+  aria2Wrapper = self.wrappers.aria2.wrap {
+    inherit pkgs;
+    settings = {
+      file-allocation = "none";
+      log-level = "warn";
+      max-connection-per-server = 4;
+      min-split-size = "5M";
+      on-download-complete = "exit";
+      auto-file-renaming = false;
+    };
+  };
+in
+pkgs.runCommand "aria2-test" { } ''
+  "${aria2Wrapper}/bin/aria2c" -v | grep "${aria2Wrapper.version}"
+  touch $out
+''
---
+++ wrapperModules/a/aria2/module.nix
@@ -0,0 +1,56 @@
+{
+  wlib,
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  formatLine =
+    n: v:
+    let
+      formatValue = v: if builtins.isBool v then (if v then "true" else "false") else toString v;
+    in
+    "${n}=${formatValue v}";
+in
+{
+  imports = [ wlib.modules.default ];
+
+  options = {
+    settings = lib.mkOption {
+      type =
+        with lib.types;
+        attrsOf (oneOf [
+          bool
+          float
+          int
+          str
+        ]);
+      default = { };
+      description = "Settings to be wrapped with aria2 binary";
+    };
+  };
+  config = {
+    package = pkgs.aria2;
+    binName = "aria2c";
+    outputs = (config.package.outputs or [ "out" ]) ++ [ "conf" ];
+    flags = {
+      "--conf-path" = "${placeholder "conf"}/${config.binName}-settings.conf";
+    };
+    flagSeparator = "=";
+    drv = {
+      renderedSettings = lib.concatStringsSep "\n" (lib.mapAttrsToList formatLine config.settings);
+      passAsFile = [ "renderedSettings" ];
+
+      buildPhase = ''
+        runHook preBuild
+        mkdir -p $conf
+        cp $renderedSettingsPath "$conf/${config.binName}-settings.conf"
+        rm $bin/bin/aria2c
+        cp $out/bin/aria2c $bin/bin
+        runHook postBuild
+      '';
+    };
+    meta.maintainers = [ wlib.maintainers.rachitvrma ];
+  };
+}
